9188dc9e6f966a189b85096716c02cf6b8015935
JUNIOR NUMEN <152480570+NUMEN901@users.noreply.github.com>
2025-02-15 21:56:32 +0100

    daily.sh

--- daily.sh
+++ daily.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#The daily.sh script is a simple Bash script that displays essential system information. 
+#It helps users quickly check the current date, logged-in user, working directory, and a list of files in the current directory.
+
 echo "Current date and time: $(date)"
 echo "Current user: $(whoami)"
 echo "Current directory: $(pwd)"
